0c06724337124749470bbeebfc85005f4eeb9b86
du-ntomazela <161898042+du-ntomazela@users.noreply.github.com>
2024-06-25 02:39:13 -0300

    Adicionando o app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+const daysElement = document.querySelector("[data-days]");
+const hoursElement = document.querySelector("[data-hours]");
+const minutesElement = document.querySelector("[data-minutes]");
+const secondsElement = document.querySelector("[data-seconds]");
+const password = document.querySelector("#password");
+const senha = 12345;
+
+const render = (days, hours, minutes, seconds) => {
+  daysElement.innerHTML = String(days).padStart("2", 0);
+  hoursElement.innerHTML = String(hours).padStart("2", 0);
+  minutesElement.innerHTML = String(minutes).padStart("2", 0);
+  secondsElement.innerHTML = String(seconds).padStart("2", 0);
+  
+};
+
+const countdown = () => {
+  const now = new Date();
+  const nextYear = now.getFullYear() ;
+  const targetDate = new Date(nextYear, 5, 19);
+
+  const timeLeft = targetDate - now;
+
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  render(days, hours, minutes, seconds);
+  
+  if(timeLeft < 0) {
+    password.innerHTML = senha;
+  }
+
+  
+};
+
+countdown();
+setInterval(countdown, 1000);
